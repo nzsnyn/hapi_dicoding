c3e00760b2e9581173c1468ccc5d0c98a50f0f62
Nathanael Zamora Satria Nugroho <672023013@student.uksw.edu>
2025-04-21 04:30:51 +0700

    git init

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addNoteHandler, getAllNotesHandler, getNoteById, updateNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteById
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: updateNoteByIdHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler
+  }
+];
+
+module.exports = routes;
